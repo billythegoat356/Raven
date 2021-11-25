01a530eee58895aba66f1f424b10021a8f015a3d
Billy <77754159+billythegoat356@users.noreply.github.com>
2021-11-25 19:42:05 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,3 +2,5 @@ python -m pip install pystyle==0.6
 python -m pip install Pillow==8.4.0
 python -m pip install requests==2.26.0
 python -m pip install imgsearch==2021.5.25
+python -m pip install bs4
+python -m pip install lxml
